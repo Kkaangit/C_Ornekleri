1a17e70c5c304e8ff2426bea537fc149f9f41e59
Kaan KELEŞ <kaankel40111@gmail.com>
2025-05-19 20:09:28 +0300

    Update deger && pointer.c

--- deger && pointer.c
+++ deger && pointer.c
@@ -1,10 +1,11 @@
     int main(){
 int *k;
 int a=10;
-k=a;
+k=&a;
         printf("'a' degeri   : %d\n",a); // 'a' degeri   : 10
         printf("a'nin adresi   : %p\n",&a); // a'nin adresi   : 0000003784bffb64
         printf("'k' degeri   : %p\n",k); // 'k' degeri   : 0000003784bffb64
         printf("'k'nin adresi : %p\n",&k); //'k'nin adresi : 0000003784bffb68
+        printf("'k' degeri   : %d\n",*k); // 'k' degeri   : 10
 return 0;
 }
