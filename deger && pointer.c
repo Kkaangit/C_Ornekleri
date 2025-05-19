972af1ab4e1e6c70bd57d254d90fef362f75b5fb
Kaan KELEŞ <kaankel40111@gmail.com>
2025-05-19 20:02:46 +0300

    Create deger && pointer.c

---
+++ deger && pointer.c
@@ -0,0 +1,10 @@
+    int main(){
+int *k;
+int a=10;
+k=a;
+        printf("'a' degeri   : %d\n",a); // 'a' degeri   : 10
+        printf("a'nin adresi   : %p\n",&a); // a'nin adresi   : 0000003784bffb64
+        printf("'k' degeri   : %p\n",k); // 'k' degeri   : 0000003784bffb64
+        printf("'k'nin adresi : %p\n",&k); //'k'nin adresi : 0000003784bffb68
+return 0;
+}
