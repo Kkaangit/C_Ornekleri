e0575c090c140bc7e513d087f28924778af0a9f0
Kaan KELEŞ <kaankel40111@gmail.com>
2025-05-20 21:39:08 +0300

    Update deger && pointer.c

--- deger && pointer.c
+++ deger && pointer.c
@@ -6,6 +6,6 @@ k=&a;
         printf("a'nin adresi   : %p\n",&a); // a'nin adresi   : 0000003784bffb64
         printf("'k' degeri   : %p\n",k); // 'k' degeri   : 0000003784bffb64
         printf("'k'nin adresi : %p\n",&k); //'k'nin adresi : 0000003784bffb68
-        printf("'k' degeri   : %d\n",*k); // 'k' degeri   : 10
+        printf("'a' degeri   : %d\n",*k); // 'a' degeri   : 10 , k değeri a'nın pointer adresini tutuyor *k yaparak o pointerda ki değeri yazar veya okur.
 return 0;
 }
